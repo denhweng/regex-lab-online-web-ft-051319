998524632df53e688e402386c057dfdaa77c0c2d
Dan Hwang <danhwangfkpc@gmail.com>
2019-05-17 19:04:39 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,10 @@
 def starts_with_a_vowel?(word)
-  word.scan(/\A[aeiouAEIOU]/) != nil 
+  word.scan(/\A[aeiouAEIOU]/) != nil
+  word.scan
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.
 end
 
 def words_five_letters_long(text)
