bc19ef7195af6606478a7bfc0d9ef3586b9a02f5
Dan Hwang <danhwangfkpc@gmail.com>
2019-05-17 19:44:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,7 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   first_letter = text.scan(/\w+/)[0]
-  if text.scan(/\./) && text.scan()
+  if text.scan(/\./) && (first_letter == first_letter.upcase)
 end
 
 def valid_phone_number?(phone)
